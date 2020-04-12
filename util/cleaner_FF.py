f5b69010fa7ecd18f3ad5be3793b0cd94f368108
franco <francomatiasferrari@gmail.com>
2020-04-12 18:38:16 -0300

    Fin limpieza nuls

--- util/cleaner_FF.py
+++ util/cleaner_FF.py
@@ -6,7 +6,7 @@ import numpy as np
 class Cleaner:
     def __init__(self):
         #Load the dataset with the inputs correlated values
-        self.dataset_values = pd.read_csv('../datos/train_sample_outliers.csv')
+        self.dataset_values = pd.read_csv('../datos/train_sample.csv')
 
     def calculate_corr_values_corr(self, col_null, col_corr, n):
         nearest_n = self.dataset_values[col_corr].iloc[(self.dataset_values[col_corr]-n).abs().argsort()[:1]].values[0]
@@ -18,13 +18,27 @@ class Cleaner:
             a = int(a)
         return a
 
-    def fill_nan_values_corr(self, df, col_null, col_corr):
+    def fill_nan_values_corr_2(self, df, col_null, col_corr):
         # Lambda functions thats apply only if the condition es fulfilled
         df['outlier'] = df[df[col_null].isnull()].apply(lambda x: self.calculate_corr_values_corr(col_null, col_corr, x[col_corr]), axis=1)
-        
+
         df[col_null].fillna(df['outlier'], inplace=True)
         df.drop(['outlier'], axis=1, inplace=True)
         df[col_null] = df[col_null].astype('int64')
+        return df
+    
+    def fill_nan_values_corr_1(self, df, col_null, col_corr):
+        # Crea una tabla con los valores mas cercanos
+        df_aux = df.copy()
+        df_aux.dropna(subset=[col_null], inplace=True)
+        tabla_nulos_temp = df_aux[[col_null,col_corr]].groupby(col_corr, as_index = False).mean().astype(int)
+        
+        # Los junta con el dataset original 
+        df = pd.merge(df, tabla_nulos_temp, how='left', on=[col_corr], suffixes=('', '_Nulls'))
+        
+        # Completa los nulls de la columna original con la auxiliar y luego la elimina
+        df[col_null].fillna(df[col_null+'_Nulls'], inplace=True)
+        df.drop([col_null+'_Nulls'], axis=1, inplace=True)
         return df
     
     def tratar_nulos_categoricos(self,df):
@@ -41,16 +55,21 @@ class Cleaner:
         return df
     
     def tratar_nulos_numericos(self,df):
-        df = self.fill_nan_values_corr(df, df.StartBattle_sum_dsi1.name, df.LoseBattle_sum_dsi1.name) # Tarda 0.02 segs por row4
-        df = self.fill_nan_values_corr(df, df.OpenChest_sum_dsi2.name, df.EnterDeck_sum_dsi2.name) # Tarda 0.02 segs por row
+        df = self.fill_nan_values_corr_1(df, df.StartBattle_sum_dsi1.name, df.LoseBattle_sum_dsi1.name) # Tabla con valores mas cercanos a corr
+        df = self.fill_nan_values_corr_1(df, df.OpenChest_sum_dsi2.name, df.EnterDeck_sum_dsi2.name) # Tabla con valores mas cercanos a corr
+        df = self.fill_nan_values_corr_2(df, df.StartBattle_sum_dsi1.name, df.LoseBattle_sum_dsi1.name) # Tarda 0.02 segs por row
+        df = self.fill_nan_values_corr_2(df, df.OpenChest_sum_dsi2.name, df.EnterDeck_sum_dsi2.name) # Tarda 0.02 segs por row
         df['ChangeArena_sum_dsi3'].fillna(0, inplace = True)
+        df['ChangeArena_sum_dsi3'] = df['ChangeArena_sum_dsi3'].astype('int64')
+        df['Label'] = df['Label'].astype('int64')
         df.drop(['age'], axis=1, inplace = True)
+
+        #Eliminar columnas no necesarias
+        df.drop(['id'], axis=1, inplace = True)
+        df.drop(['Label_max_played_dsi'], axis=1, inplace = True)
         return df
     
     def clean_all(self,df):
         df = self.tratar_nulos_numericos(df)
         df = self.tratar_nulos_categoricos(df)
         return df
-
-
-
